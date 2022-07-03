83b53ab32873070c087514780ecd0eaebb5c1c7d
shreybohra <42218014+shreybohra@users.noreply.github.com>
2022-07-03 01:18:10 +0100

    unpack bin

--- APEX I/data_processing.m
+++ APEX I/data_processing.m
@@ -3,4 +3,15 @@ clear
 
 launch = 'APEXI-jul02';
 [t, alt, vel, temp, voltage] = pnut_extractor(launch);
+raw = unpack(launch);
+
+
+function data = unpack(launch)
+
+    command = sprintf("python parser.py %s-custom", launch);
+    fprintf('Parsing data from %s launch \n', launch);
+    status = system(command);
+    data = readmatrix(sprintf('%s-custom_parsed.csv', launch));
+    
+end
 
