4bb11547195f76ae93822b47824f098918919381
Shrey Bohra <42218014+shreybohra@users.noreply.github.com>
2024-04-15 00:53:06 +0100

    metric conversion

--- ASTRA V/raven_plot.py
+++ ASTRA V/raven_plot.py
@@ -5,6 +5,11 @@ import plotly.graph_objects as go
 import numpy as np
 
 
+# say no to freedom units
+ft_to_m = 0.3048
+
+def toCelsius(f):
+    return (f - 32) * 5/9
 
 #%%
 flight = "sustainer"
@@ -16,5 +21,13 @@ lr = pd.read_csv(lr_fname)
 hr = pd.read_csv(hr_fname)
 
 #%%
-lr.head()
-hr.head()
+
+lr["Temperature_(F)"] = lr["Temperature_(F)"].apply(toCelsius)
+lr["Baro_Altitude_ASL_(feet)"] = lr["Baro_Altitude_ASL_(feet)"] * ft_to_m
+lr["GPS_Altitude_AGL_(feet)"] = lr["GPS_Altitude_AGL_(feet)"] * ft_to_m
+
+col_map = {"Temperature_(F)": "Temperature", 
+           "Baro_Altitude_ASL_(feet)": "Baro_Altitude_ASL", 
+           "Baro_Altitude_AGL_(feet)": "Baro_Altitude_AGL"}
+
+lr = lr.rename(columns=col_map)
